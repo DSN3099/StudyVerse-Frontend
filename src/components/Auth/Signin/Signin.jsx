9dbfe3344f1c25d4f09b7b48b7a05de886d9a7e8
ayushsingh0607 <ayushraj0607@gmail.com>
2022-11-09 12:55:35 +0530

    ayush commits full responsive sigin page

--- src/components/Auth/Signin/Signin.jsx
+++ src/components/Auth/Signin/Signin.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 
 const Signin = () => {
   const navigate = useNavigate();
-  const [seepassword,setSeepassword] = useState(false);
+  const [seepassword, setSeepassword] = useState(false);
 
   const handleChange = () => {
     navigate('/signup')
@@ -42,61 +42,56 @@ const Signin = () => {
             <div class='border-x-8 bg-slate-500 h-px w-1/2'></div>
           </div>
           {/* <div> */}
-            <div class='flex flex-col gap-4 '>
-              <form class='flex flex-col gap-4 '>
-                <div class='flex flex-col gap-2'>
-                  <span class='text-slate-600 text-1xl font-bold'>Email</span>
-                  <div class='flex border-2 border-slate-600 rounded-md w-full items-center'>
-                    <div class='flex items-center  ml-2'>
+          <div class='flex flex-col gap-4 '>
+            <form class='flex flex-col gap-4 '>
+              <div class='flex flex-col gap-2'>
+                <span class='text-slate-600 text-1xl font-bold'>Email</span>
+                <div class='flex border-2 border-slate-600 rounded-md w-full items-center'>
+                  <div class='flex items-center  ml-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                    </svg>
+                    <input class='p-2.5 text-gray-700 font-semibold outline-none w-full  ' type="text" placeholder='Enter your Email' />
+                  </div>
+                </div>
+              </div>
+              <div class='flex flex-col gap-2'>
+                <span class='text-slate-600 text-1xl font-bold'>Password</span>
+                <div class='flex border-2 border-slate-600 rounded-md w-full items-center '>
+                  <div class='flex items-center justify-between w-full ml-2'>
+                    <div class='flex items-center '>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                       </svg>
-                      <input class='p-2.5 text-gray-700 font-semibold outline-none w-full  ' type="text" placeholder='Enter your Email' />
+                      <input class='p-2.5 text-gray-700 font-semibold outline-none w-full ' type={seepassword ? 'text' : 'password'} placeholder='Enter your Password' />
                     </div>
+                    <div class='pr-2.5 cursor-pointer' onClick={() => { setSeepassword(!seepassword) }}>{!seepassword ? <VisibilityOffIcon /> : <VisibilityIcon />}</div>
                   </div>
                 </div>
-                <div class='flex flex-col gap-2'>
-                  <span class='text-slate-600 text-1xl font-bold'>Password</span>
-                  <div class='flex border-2 border-slate-600 rounded-md w-full items-center '>
-                    <div class='flex items-center justify-between w-full ml-2'>
-                      <div class='flex items-center '>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                        </svg>
-                        <input class='p-2.5 text-gray-700 font-semibold outline-none w-full ' type={seepassword ? 'text' : 'password'} placeholder='Enter your Password' />
-                      </div>
-                      <div class='pr-2.5 cursor-pointer' onClick={() => { setSeepassword(!seepassword) }}>{!seepassword ? <VisibilityOffIcon /> : <VisibilityIcon  />}</div>
-                    </div>
-                  </div>
-                </div>
-              </form>
-              <div class='flex justify-between w-full'>
-                <div class='flex items-center  gap-2'>
-                  <input class='w-4 h-4 ' type="checkbox" />
-                  <span class='text-xl text-slate-600'>Remember Me</span>
-                </div>
-                <div class='flex  cursor-pointer '>
-                  <span class='text-xl text-slate-600'>Forgot Password?</span>
-                </div>
               </div>
-              <div>
-                <button class='flex items-center justify-center  gap-2 bg-blue-600 w-full p-2.5 rounded-md'>
-                  <span class='text-2xl text-white'>Sign in</span>
-                </button>
+            </form>
+            <div class='flex justify-between w-full'>
+              <div class='flex items-center  gap-2'>
+                <input class='w-4 h-4 ' type="checkbox" />
+                <span class='text-xl text-slate-600'>Remember Me</span>
               </div>
-              <div class='flex justify-center gap-2 mb-4'>
-                <span class='cursor-pointer' onClick={handleChange}>Don't Have An Account?</span>
-                <span class='text-sky-500 cursor-pointer' onClick={handleChange}>Sign up</span>
+              <div class='flex  cursor-pointer '>
+                <span class='text-xl text-slate-600'>Forgot Password?</span>
               </div>
             </div>
-          {/* </div> */}
-
+            <div>
+              <button class='flex items-center justify-center  gap-2 bg-blue-600 w-full p-2.5 rounded-md'>
+                <span class='text-2xl text-white'>Sign in</span>
+              </button>
+            </div>
+            <div class='flex justify-center gap-2 mb-4'>
+              <span class='cursor-pointer' onClick={handleChange}>Don't Have An Account?</span>
+              <span class='text-sky-500 cursor-pointer' onClick={handleChange}>Sign up</span>
+            </div>
+          </div>
         </div>
-    <div class='flex-col h-full'>
-      <div class='mt-4'>
-        <img src={logo} alt="logo" class='w-48 h-24' />
-        {/* <img src={back} alt="" /> */}
       </div>
+
     </div>
   )
 }
