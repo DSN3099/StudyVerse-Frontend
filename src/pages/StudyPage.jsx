2eb1c992b03302c045f289a36b140c3e29fbeb96
Yashvardhans <yashvardhans016@gmail.com>
2022-11-17 23:07:30 +0530

    made few changes by yash

--- src/pages/StudyPage.jsx
+++ src/pages/StudyPage.jsx
@@ -2,6 +2,10 @@
 import Navbar from '../components/Navbar/Navbar';
 import ReactPlayer from "react-player";
 import star from '../assets/star.png';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import DoneIcon from '@mui/icons-material/Done';
+import { green } from '@mui/material/colors';
 
 
 
@@ -9,27 +13,33 @@ import star from '../assets/star.png';
 const sessionArray = [
     {   
         id : 1 ,
-        name : "Lorem Ipsum is simply dummy text of  "
+        name : "Lorem Ipsum is simply dummy text of  ",
+        url : "https://www.youtube.com/watch?v=5SXK__rm6DM&list=PLSzsOkUDsvds51lVJYzKWhqr4Ie6UHLx7&index=1"
     },
     {   
         id : 2 ,
-        name : "Lorem Ipsum is simply dummy text of  "
+        name : "Lorem Ipsum is simply dummy text of  ",
+        url : "https://www.youtube.com/watch?v=tWslE3uejuI&list=PLSzsOkUDsvds51lVJYzKWhqr4Ie6UHLx7&index=2"
     },
     {   
         id : 3 ,
-        name : "Lorem Ipsum is simply dummy text of "
+        name : "Lorem Ipsum is simply dummy text of ",
+        url : "https://www.youtube.com/watch?v=tWslE3uejuI&list=PLSzsOkUDsvds51lVJYzKWhqr4Ie6UHLx7&index=3"
     },
     {   
         id : 4 ,
-        name : "Lorem Ipsum is simply dummy text of"
+        name : "Lorem Ipsum is simply dummy text of",
+        url : "https://www.youtube.com/watch?v=tWslE3uejuI&list=PLSzsOkUDsvds51lVJYzKWhqr4Ie6UHLx7&index=4"
     },
     {   
         id : 5 ,
-        name : "Lorem Ipsum is simply dummy text of"
+        name : "Lorem Ipsum is simply dummy text of",
+        url : "https://www.youtube.com/watch?v=tWslE3uejuI&list=PLSzsOkUDsvds51lVJYzKWhqr4Ie6UHLx7&index=5"
     },
     {   
         id : 6 ,
-        name : "Lorem Ipsum is simply dummy text of "
+        name : "Lorem Ipsum is simply dummy text of ",
+        url : "https://www.youtube.com/watch?v=tWslE3uejuI&list=PLSzsOkUDsvds51lVJYzKWhqr4Ie6UHLx7&index=6"
     }
 ];
 
@@ -37,15 +47,16 @@ const sessionArray = [
 
 function Study(){
     return(
-        
+       <div> 
+        <Navbar type = "verified"></Navbar>
         <div className='studyPage mx-20'>
-            <Navbar type = "verified"></Navbar>
+            
             <div className='mb-4 text-sm'>My course/In Progress</div>
             <div className='course_header flex'>
                <div class='font-bold text-4xl mb-1 flex-1'>UI Design ,A User Centered Approach</div>
                <div className='button_grp flex-1 flex  justify-end pr-5 w-80'>
-                            <button className=' bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded w-24 '>Share</button>
-                            <button className='ml-4 bg-indigo-200 hover:bg-indigo-700 hover:text-white text-[#535CE8FF] font-bold py-2 px-4 rounded w-24'>Save</button>
+                           <button className=' bg-indigo-200 hover:bg-indigo-700 hover:text-white text-[#535CE8FF] font-bold py-2 px-4 rounded  w-26 px-2'>< ShareIcon className='pr-2 pb-1'></ShareIcon>Share</button>
+                            <button className='ml-4 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded w-26 px-2 '><FavoriteIcon className='pr-2 pb-1'></FavoriteIcon>Save</button>
                 </div>
             </div>
            
@@ -59,18 +70,18 @@ function Study(){
             </div>       
             <div className='flex flex-row'>
                 <div className='course_player '>
-                   <ReactPlayer width={808} height = {416} controls url = "https://www.youtube.com/watch?v=-6_7NywjE6I"></ReactPlayer>
+                   <ReactPlayer width={808} height = {416} controls url = {sessionArray[0].url}></ReactPlayer>
                 </div>
                 <div className='sessions ml-20'>
                     <div className='sessions_header flex'>
-                        <div className= 'flex-1 text-xl text-[#535CE8FF] pl-2.5'>Sessions</div>
+                        <div className= 'flex-1 text-xl text-[#535CE8FF] pl-2.5 font-bold'>Sessions</div>
                         <div className='flex-1 text-sm pt-1 font-bold'>3/12 Completed</div>
                        
                     </div>
                     
                     <div className="session_list mt-5" style={{width : '400px' , height : 'auto'  }}>
                         {sessionArray.map(session => (
-                           <div className='p-3.5  text-sm my-1.5 hover:border-[3px] hover:border-indigo-500' >0{session.id}. {session.name}</div>
+                           <div className='p-3.5  text-sm my-1.5 hover:border-[3px] hover:border-indigo-500' >0{session.id}. {session.name} <DoneIcon className='ml-12 mr-0' style={{fill:green}}></DoneIcon></div>
                            ) )}
                     </div>
                  </div>
@@ -78,6 +89,7 @@ function Study(){
             </div>
             
         </div>
+    </div> 
     )
 };
 
