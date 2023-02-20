f0639914e3adc9bbe07e198fd4409b39db00412b
ayushsingh0607 <ayushraj0607@gmail.com>
2023-02-20 20:50:28 +0530

    ayush commits integrated courseupload.

--- src/pages/Courseupload.jsx
+++ src/pages/Courseupload.jsx
@@ -3,31 +3,70 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { useRef } from 'react';
 import file from '../assets/file.png';
-// import loader from '../assets/loader1.gif';
+import loader from '../assets/loader1.gif';
 import edit from '../assets/editvideos.svg';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { v4 } from "uuid"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "../Firebase.js"
 import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Courseupload = () => {
 
-    const data = [
-        { id: 1, name: 'FileName:1', size: "20Mb" },
-        { id: 2, name: 'FileName:2', size: "20Mb" },
-        { id: 3, name: 'FileName:3', size: "20Mb" },
-        { id: 4, name: 'FileName:4', size: "20Mb" },
-        { id: 5, name: 'FileName:5', size: "20Mb" },
-        { id: 6, name: 'FileName:6', size: "20Mb" },
-        { id: 7, name: 'FileName:7', size: "20Mb" },
-        { id: 8, name: 'FileName:8', size: "20Mb" },
-        { id: 9, name: 'FileName:9', size: "20Mb" },
-        { id: 10, name: 'FileName:10', size: "20Mb" },
-        { id: 11, name: 'FileName:11', size: "20Mb" },
-    ]
 
-    const [fileupload,setFileupload] = useState();
+    const [fileupload, setFileupload] = useState([]);
+    const [coursedata, setCoursedata] = useState()
+    const [initial, setInitial] = useState(true)
     const [edittitle, setEdittitle] = useState(false);
-    const [editid,setEditid] = useState();
-    const [editname,setEditname] = useState();
+    const [editid, setEditid] = useState();
+    const [editname, setEditname] = useState();
+
+    const navigate = useNavigate();
+
+
+
+    const { id } = useParams();
+    const addvideos = async (Data) => {
+        try {
+            const { data } = await axios.post(`http://localhost:5000/api/video/${id}`, Data)
+            console.log(data)
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    const handleChange = async (e) => {
+        const file = e.target.files[0];
+        setFileupload([...fileupload, file])
+        // console.log(fileupload)
+        const fileid = v4()
+        const storageref = ref(storage, `video/${file.name + fileid}`)
+        await uploadBytes(storageref, file)
+        const url = await getDownloadURL(storageref)
+        const Data = { id: fileid, name: file.name, size: file.size, url }
+        // console.log(url)
+        addvideos(Data)
+        alert('uploaded..')
+    }
+
+    const getvideos = async () => {
+        try {
+            const { data } = await axios.get(`http://localhost:5000/api/course/${id}`)
+            setFileupload(data.lessons)
+            setCoursedata(data)
+            console.log(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        if (initial) {
+            getvideos()
+            setInitial(false)
+        }
+    }, [initial])
 
     const choose = useRef();
     return (
@@ -36,11 +75,12 @@ const Courseupload = () => {
                 <div class='flex flex-1 items-center justify-center w-full'>
                     <div class='flex flex-col gap-5 items-center w-1/2'>
                         <div class='flex flex-col gap-2 items-center'>
+                            <span class='text-4xl mb-10 font-semibold '>{coursedata?.title}</span>
                             <span class='text-[22px] font-bold text-[#292929]'>Upload the videos</span>
                             <span class='text-[12px] text-[#A0A0A0] '>File should be mp4</span>
                         </div>
                         <div class=' h-[274px] bg-[#FBFBFF] rounded-md flex items-center'>
-                            <input type="file" hidden ref={choose} accept='video/*'/>
+                            <input type="file" hidden ref={choose} accept='video/*' onChange={handleChange} />
                             <Button variant='contained' sx={{ textTransform: 'capitalize', cursor: 'pointer' }} onClick={() => { choose.current.click() }}>Upload Files</Button>
                         </div>
                     </div>
@@ -50,15 +90,15 @@ const Courseupload = () => {
                     <div class='flex flex-col gap-5 w-full'>
                         <span class='text-[#A0A0A0] text-[14px]  font-[400]'>Uploaded Files</span>
                         <div class='flex flex-col gap-3 overflow-y-auto h-[550px]'>
-                            {data.map((values, i) => (
-                                <div class='border-2 border-[#698AFF] p-2 rounded-md' onClick={()=>{setEditid(values.id)}}>
+                            {fileupload.map((values, i) => (
+                                <div class='border-2 border-[#698AFF] p-2 rounded-md ' onClick={() => { setEditid(values.id) }}>
                                     <div class='flex justify-between'>
                                         <div class='flex gap-1'>
                                             <img src={file} alt="file" />
-                                            <div class='flex flex-col'>
+                                            <div class='flex flex-col justify-center'>
                                                 <span class='text-[14px] text-[#001356] font-[400]'>{values.name}</span>
-                                                {/* <img src={loader} alt="loader" class=''  /> */}
-                                                <span class='text-[12px] text-[#001356] font-[400]'>{values.size}</span>
+                                                {/* <img src={loader} alt="loader" class='w-1/2 h-'  /> */}
+                                                <span class='text-[12px] text-[#001356] font-[400]'>{Math.round((values.size) / 1000000)}MB</span>
                                             </div>
                                         </div>
                                         <div class='flex gap-1 items-center'>
@@ -68,15 +108,15 @@ const Courseupload = () => {
                                     </div>
                                     {edittitle && (values.id === editid) &&
                                         <div class='w-full flex gap-2 mt-2'>
-                                            <TextField size="small" label='Edit here' sx={{ width: "100%" }} onChange={(e)=>{setEditname(e.target.values)}}></TextField>
-                                            <Button variant='contained' sx={{ textTransform: "capitalize", padding: '6px 20px' }} onClick={()=>{setEdittitle(false)}}>Update</Button>
+                                            <TextField size="small" label='Edit here' sx={{ width: "100%" }} onChange={(e) => { setEditname(e.target.values) }}></TextField>
+                                            <Button variant='contained' sx={{ textTransform: "capitalize", padding: '6px 20px' }} onClick={() => { setEdittitle(false) }}>Update</Button>
                                         </div>
                                     }
                                 </div>
                             ))}
                         </div>
                         <div class='flex justify-end'>
-                            <Button variant='contained' sx={{ padding: '8px 30px' }}>Go to Course</Button>
+                            <Button variant='contained' sx={{ padding: '8px 30px' }} onClick={() => { navigate(`/video/${id}`) }}>Go to Course</Button>
                         </div>
                     </div>
                 </div>
@@ -85,3 +125,4 @@ const Courseupload = () => {
         </div>
     )
 }
+export default Courseupload;
