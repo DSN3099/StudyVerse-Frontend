7fe2c01b487424ac44cc717a4cb6d3a620040434
Yashvardhans <yashvardhans016@gmail.com>
2023-03-05 14:10:53 +0530

    yash commit frontend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import {Route,Routes} from 'react-router-dom';
 import Home from './pages/Home'
 import Signup from './components/Auth/SignUp/Signup';
 import Signin from './components/Auth/Signin/Signin';
 import CourseInfo from './pages/CourseInfo';
 import Landing from './pages/Landing';
-import Courseupload from './pages/Courseupload';
 import Study from "./pages/StudyPage";
-import CreateCourse from './components/CreateCourse';
-import TeachersPage from './pages/TeachersPage';
 function App() {
   return (
-    <div className="App ">
-        <Routes>
-          {/* <Route exact path="/" element={<Landing />}></Route> */}
-          <Route exact path="/" element={<TeachersPage/>}></Route>
-          <Route exact path="/courseinfo/:id" element={<CourseInfo />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/signin" element={<Signin />}></Route>
-          <Route path="/video/:id" element={<Study />}></Route>
-          <Route path='/createcourse' element = {<CreateCourse />}></Route>
-          <Route path='/videoupload/:id' element = {<Courseupload />}></Route>
-        </Routes>
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Landing />}></Route>
+        <Route exact path="/courseinfo:id" element={<CourseInfo />}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/signin" element={<Signin/>}></Route>
+        <Route path = "/study" element = {<Study/>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/StudyPage.jsx
+++ src/pages/StudyPage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar/Navbar';
 import ReactPlayer from "react-player";
 import star from '../assets/star.png';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import ShareIcon from '@mui/icons-material/Share';
 import DoneIcon from '@mui/icons-material/Done';
 import { useState } from 'react';
@@ -49,6 +50,30 @@ const sessionArray = [
 function Study(){
 const [url , setUrl] = useState("https://www.youtube.com/watch?v=7xEgFfpUeEs");
 const [color , setColor] = useState(false);
+const [active, setActive] = useState(0);
+const [playing , setPlaying] = useState(true);
+const [progress , setProgress] = useState(0);
+
+const handleColor = (level) =>{
+    setColor(!color);
+    
+    console.log(level);
+    
+
+}
+const handlePlay = (url , level) =>{
+    setPlaying(!playing);
+    setUrl(url);
+    setActive(level);
+    console.log(playing);
+   
+
+   
+}
+
+
+
+
 
 
 
@@ -59,37 +84,37 @@ const [color , setColor] = useState(false);
             
             <div className='mb-4 text-sm'>My course/In Progress</div>
             <div className='course_header flex'>
-               <div class='font-bold text-4xl mb-1 flex-1'>UI Design ,A User Centered Approach</div>
-               <div className='button_grp flex-1 flex  justify-end pr-5 w-80'>
-                           <button className=' bg-indigo-200 hover:bg-indigo-700 hover:text-white text-[#535CE8FF] font-bold py-2 px-4 rounded  w-26 px-2'>< ShareIcon className='pr-2 pb-1'></ShareIcon>Share</button>
-                            <button className='ml-4 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded w-26 px-2 '><FavoriteIcon className='pr-2 pb-1'></FavoriteIcon>Save</button>
+               <div class='font-bold text-[32px] mb-1 flex-1'>UI Design ,A User Centered Approach</div>
+               <div className='button_grp flex-1 flex  justify-end pr-5 w-[50%] mr-[65px] mt-[20px]'>
+                        <button className=' bg-indigo-200 hover:bg-indigo-700 hover:text-white text-[#535CE8FF] font-bold py-2 px-4 rounded  w-[97px] px-2 h-[36px] text-[14px]'>< ShareIcon className='pr-2 pb-1'></ShareIcon>Share</button>
+                        <button className='ml-2 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded w-[92px] h-[36px] px-2 text-[14px] '><FavoriteIcon className='pr-2 pb-1'></FavoriteIcon>Save</button>
                 </div>
             </div>
            
             <div class='flex w-full items-center justify-between mb-3'>
-                    <div class='flex gap-1 w-max p-2'>
-                        <div class='text-blue-600 cursor-pointer border-r-2 pr-2'>Klara Weaver</div>
+                    <div class='flex gap-1 w-max'>
+                        <div class='text-blue-600 cursor-pointer border-r-2 pr-1'>Klara Weaver</div>
                         <img src={star} alt="" style={{ width: '20px', height: '20px' }} className="mx-2"/>
                         <div class='flex font-bold'>4.5<span class='font-normal text-gray-500 border-r px-2'>(99 reviews)</span></div>
                         
                     </div>
             </div>       
-            <div className='flex flex-row'>
+            <div className='flex flex-row justify-between' >
                 
-                <div className='course_player '>
-                   <ReactPlayer width={808} height = {416} controls url = {url}></ReactPlayer>
+                <div className='course_player w-full'>
+                   <ReactPlayer width={"100%"} height = {"100%"} controls playing = {!playing} url = {url} onEnded = {() => handleColor}></ReactPlayer>
                 </div>
                 <div className='sessions ml-20'>
                     <div className='sessions_header flex'>
-                        <div className= 'flex-1 text-xl text-[#535CE8FF] pl-2.5 font-bold'>Sessions</div>
+                        <div className= 'flex-1 text-[20px] text-[#535CE8FF] pl-2.5 font-bold'>Sessions</div>
                         <div className='flex-1 text-sm pt-1 font-bold'>3/12 Completed</div>
                        
                     </div>
                     
                     <div className="session_list mt-5" style={{width : '400px' , height : 'auto'  }}>
                         {sessionArray.map(session => (
-                          <button className='p-3.5  text-sm my-1.5 hover:border-[3px] hover:border-indigo-500' onClick={()=>{setUrl(session.url); setColor(true);console.log(url); }}>
-                            0{session.id}. {session.name} <DoneIcon className={color ? "bg-green-500" : "bg-violet-500" }></DoneIcon></button>
+                          <button className='p-3.5  text-sm my-1.5 hover:border-[3px] hover:border-indigo-500' onClick={()=>handlePlay(session.url , session.id)}  >
+                            0{session.id}. {session.name}{playing && active == session.id ? <PlayArrowIcon className="text-red-500"></PlayArrowIcon> : <DoneIcon className={color ? "text-green-500" : ""}> </DoneIcon>}</button>
 
                            ) )}
                     </div>
